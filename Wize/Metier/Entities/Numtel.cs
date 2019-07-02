c9132bf775aa74a4b47bb6b664c0a131eea1f0ff
loujulien <laetitia_julien@live.fr>
2019-07-02 15:51:19 +0200

    ajout de l'entité Numtel

---
+++ Wize/Metier/Entities/Numtel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metier.Entities
+{
+    class Numtel
+    {
+        //TEXT(1,25)
+        string numtel {get; set;}
+    }
+}
