b6772c478e1c614c79367f28370333e258542a27
loujulien <laetitia_julien@live.fr>
2019-07-02 16:53:26 +0200

    bascule des entitées cree sur la dev

---
+++ Wize/Metier/Entities/Certification.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metier.Entities
+{
+    public class Certification
+    {
+        //TEXT(1,6)
+        public string codeRNCP {get; set;}
+        //TEXT(1,6)
+        public string codeCERTIFINFO {get; set;}
+    }
+}
---
+++ Wize/Metier/Entities/Numtel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metier.Entities
+{
+    public class Numtel
+    {
+        //TEXT(1,25)
+        public string numtel {get; set;}
+    }
+}
