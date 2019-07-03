72fa3c01ac98fbc45447dde6017a384928a16422
loujulien <laetitia_julien@live.fr>
2019-07-03 12:16:38 +0200

    correction espaces dans formation.cs

--- Wize/Metier/Entities/Formation.cs
+++ Wize/Metier/Entities/Formation.cs
@@ -7,49 +7,49 @@ namespace Metier.Entities
     public class Formation
     {
         //DomaineFormation
-        public DomaineFormation domaineFormation {get; set;}
+        public DomaineFormation domaineFormation { get; set; }
         //TEXT(1,255) non nul
-        public string intituleFormation {get; set;}
+        public string intituleFormation { get; set; }
         //TEXT(1,3000) non nul
-        public string objectifFormation {get; set;}
+        public string objectifFormation { get; set; }
         //TEXT(1,3000) non nul
-        public string resultatsAttendus {get; set;}
+        public string resultatsAttendus { get; set; }
         //TEXT(1,3000) non nul
-        public string contenuFormation {get; set;}
+        public string contenuFormation { get; set; }
         //CODE(dict-boolean) non nul
-        public bool certifiante {get; set;}
+        public bool certifiante { get; set; }
         //coordonnees non nul
-        public Coordonnees contactFormation {get; set;}
+        public Coordonnees contactFormation { get; set; }
         //CODE(dict-type-parcours) non nul
-        //public int parcoursDeFormation {get; set;}
+        //public int parcoursDeFormation { get; set; }
         //CODE(dict-niveaux) non nul
-        public int codeNiveauEntree {get; set;}
+        public int codeNiveauEntree { get; set; }
         //CODE(dict-AIS)
-        //public int objectifGeneralFormation {get; set;}
+        //public int objectifGeneralFormation { get; set; }
         //certification
-        public Certification certification {get; set;}
+        public Certification certification { get; set; }
         //web
-        public Web urlFormation {get; set;}
+        public Web urlFormation { get; set; }
         //action
-        public Action action {get; set;}
+        public Action action { get; set; }
         //organisme-formation-responsable
-        public OrganismeFormationResponsable organismeFormationResponsable {get; set;}
+        public OrganismeFormationResponsable organismeFormationResponsable { get; set; }
         //CODE(dict-niveaux)
-        //public int codeNiveauSortie {get; set;} 
+        //public int codeNiveauSortie { get; set; } 
         //TEXT(1,3000)
-        public string identifiantModule {get; set;}
+        public string identifiantModule { get; set; }
         //CODE(dict-type-positionnement)
-        //public int positionnement {get; set;}
+        //public int positionnement { get; set; }
         //sous-module
-        public SousModule sousModule {get; set;}
+        public SousModule sousModule { get; set; }
         //modules-prerequis
-        public ModulePrerequis modulesPrerequis {get; set;}
+        public ModulePrerequis modulesPrerequis { get; set; }
         //INT
-        public int creditsEcts {get; set;}
+        public int creditsEcts { get; set; }
         //BOOL
-        public bool eligibiliteCpf {get; set;}
+        public bool eligibiliteCpf { get; set; }
         //TEXT
-        public string validations {get; set;}
+        public string validations { get; set; }
     }
 }
 
