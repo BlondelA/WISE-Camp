1ef3af46b8fbca952abda3b5713c56b2a43b563d
loujulien <laetitia_julien@live.fr>
2019-07-03 10:16:46 +0200

    creation EntiteModulePrerequis

---
+++ Wize/Metier/Entities/ModulesPrerequis.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metier.Entities
+{
+    public class ModulesPrerequis
+    {
+        //TEXT(1,3000) non nul
+        public string referenceModule {get; set;}
+        
+    }
+}
