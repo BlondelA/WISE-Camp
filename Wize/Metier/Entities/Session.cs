066aa25157f20d268688eb89ce6e45d8b8e89b2c
Antoine Blondel <mail>
2019-07-03 11:37:56 +0200

    Correction Session entity
    - nom variables
    - espace avant get set

--- Wize/Metier/Entities/Session.cs
+++ Wize/Metier/Entities/Session.cs
@@ -7,13 +7,13 @@ namespace Metier.Entities
     public class Session
     {
         // periode 
-        public Periode periode{get; set;}
+        public Periode periode {get; set;}
         // adresse
-        public Adresse adresse_inscription {get; set;}
+        public Adresse adresseInscription {get; set;}
         // periode
-        public Periode periode_inscription {get; set;}
+        public Periode periodeInscription {get; set;}
         // TEXT(1,255)
-        public string modalite_inscription {get; set;}
+        public string modaliteInscription {get; set;}
         // CODE(dict-etat-recrutement)
         //public etat-recrutement etat_recrutement { get; set; }
     }
