ed061f671bbe565c45664d7cdeb51396f10a53e1
Pierrick <pierrick.besnard@imie.fr>
2019-07-03 10:31:20 +0200

    Ajout de l'entitee sous-module

---
+++ Wize/Metier/Entities/SousModule.cs
@@ -0,0 +1,8 @@
+namespace Metier.Entities
+{
+    public class SousModule
+    {
+        public string referenceModule; // TEXT (0 , 3000)
+        public int typeModule; // CODE (dict-type-module)
+    }
+}
