302c1448a9f2f0714a3f38a2eade1ab0eb7bd1d6
Antoine Blondel <mail>
2019-07-03 10:19:47 +0200

    Domaine Formation Entity Creation

---
+++ Wize/Metier/Entities/DomaineFormation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metier.Entities
+{
+    public class DomaineFormation
+    {
+        // TEXT (5,5)
+        public string code_FORMACODE { get; set; }
+        // TEXT (3,3)
+        public string code_NSF { get; set; }
+        // TEXT (5,5)
+        public string code_ROME { get; set; }
+    }
+}
