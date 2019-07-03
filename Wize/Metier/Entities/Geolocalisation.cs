7635f8b09942660fc0e9e062dc6c1508418e1b5d
Antoine Blondel <mail>
2019-07-03 09:20:29 +0200

    Geolocalisation Entity Creation

---
+++ Wize/Metier/Entities/Geolocalisation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metier.Entities
+{
+    public class Geolocalisation
+    {
+        // TEXT (0,30)
+        public string latitude { get; set; }
+        // TEXT (0,30)
+        public string longitude { get; set; }
+    }
+}
