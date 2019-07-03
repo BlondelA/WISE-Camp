964dc8e692e5202e3cdca53fa264d66c19fa7953
Antoine Blondel <mail>
2019-07-03 09:46:21 +0200

    Session Entity Creation

---
+++ Wize/Metier/Entities/Session.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metier.Entities
+{
+    public class Session
+    {
+        // periode 
+        //public Periode periode{get; set;}
+        // adresse
+        //public Adresse adresse_inscription {get; set;}
+        // periode
+        //public Periode periode_inscription {get; set;}
+        // TEXT(1,255)
+         public string modalite_inscription {get; set;}
+        // CODE(dict-etat-recrutement)
+       //public etat-recrutement etat_recrutement { get; set; }
+    }
+}
