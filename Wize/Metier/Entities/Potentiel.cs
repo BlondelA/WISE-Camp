c4f02d49ba85200ac848b062a28e6ff8c1f76bdc
Antoine Blondel <mail>
2019-07-03 10:03:23 +0200

    Potentiel Entity Creation

---
+++ Wize/Metier/Entities/Potentiel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metier.Entities
+{
+    public class Potentiel
+    {
+        // TEXT (5,5)
+        public string code_FORMACODE { get; set; }
+    }
+}
