ef33fedd03c6bb482506f0c4b2ea713c2157d3f2
Antoine Blondel <mail>
2019-07-04 16:12:19 +0200

    Creation User & Role entity + model

---
+++ Wize/Metier/Entities/Role.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metier.Entities
+{
+    public class Role
+    {
+        // TEXT
+        public string identifiant { get; set; }
+    }
+}
---
+++ Wize/Metier/Entities/Utilisateur.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metier.Entities
+{
+    public class Utilisateur
+    {
+        // TEXT 
+        public string nom { get; set; }
+        // TEXT
+        public string prenom { get; set; }
+        // TEXT
+        public string mail { get; set; }
+        // TEXT
+        public string motDePasse { get; set; }
+        // role
+        public Role role { get; set; }
+    }
+}
