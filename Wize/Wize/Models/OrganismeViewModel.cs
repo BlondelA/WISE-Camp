863b0ee7934c42ea7b78662b2380f27e7943eacd
loujulien <laetitia_julien@live.fr>
2019-07-04 16:07:58 +0200

    ajout gestion du Json

--- Wize/Wize/Models/OrganismeViewModel.cs
+++ Wize/Wize/Models/OrganismeViewModel.cs
@@ -1,14 +1,20 @@
-﻿namespace Wize.Models
+﻿using Newtonsoft.Json;
+
+namespace Wize.Models
 {
     public class OrganismeViewModel
     {
         // TEXT (14,14) non null
+        [JsonProperty(PropertyName = "SIRET")]
         public string siret { get; set; }
         // TEXT (1,250) non null
+        [JsonProperty(PropertyName = "raison-sociale")]
         public string raisonSociale { get; set; }
         // Coordonnées
+        [JsonProperty(PropertyName = "contact-organisme")]
         public CoordonneesViewModel contactOrganisme { get; set; }
         // potentiel
+        [JsonProperty(PropertyName = "potentiel")]
         public PotentielViewModel potentiel { get; set; }
     }
 }
